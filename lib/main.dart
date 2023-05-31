b42bfa6949542967a9cb0aec511cade7e4c2d1c8
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2023-05-31 12:46:19 +0300

    first functional fully attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-//import 'package:dio/dio.dart';
+import 'package:web_socket_channel/io.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,96 +12,157 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NodeMCU Control',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: "NodeMCU Controller",
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.blue, // Set the app bar color to blue
+        ),
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  MyHomePageState createState() => MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
 }
 
-class MyHomePageState extends State<MyHomePage> {
-  final String nodeMCUIpAddress = '192.168.1.105'; // Replace with your NodeMCU's IP address
-  final int nodeMCUPort = 80; // Replace with the port number used in your NodeMCU code
-  bool isLEDTurnedOn = false;
-  String currentTemperature = "0.0";
+class _HomePageState extends State<HomePage> {
+  late String temp;
+  late IOWebSocketChannel channel;
+  late bool connected;
+  late bool ledOn;
+
+  @override
+  void initState() {
+    connected = false;
+    ledOn = false;
+    temp = "N/A";
+    Future.delayed(Duration.zero, () async {
+      channelConnect();
+    });
+
+    super.initState();
+  }
+
+  channelConnect() {
+    try {
+      channel = IOWebSocketChannel.connect("ws://192.168.0.1:81");
+      channel.stream.listen((message) {
+        if (kDebugMode) {
+          print(message);
+        }
+        setState(() {
+          if (message == "connected") {
+            connected = true;
+          } else if (message.substring(0, 8) == "{'temp':") {
+            message = message.replaceAll(RegExp("'"), '"');
+            Map<String, dynamic> jsonData = json.decode(message);
+            setState(() {
+              temp = jsonData["temp"];
+            });
+          }
+        });
+      }, onDone: () {
+        if (kDebugMode) {
+          print("Web socket is closed");
+        }
+        setState(() {
+          connected = false;
+          temp = "N/A";
+        });
+      }, onError: (error) {
+        if (kDebugMode) {
+          print(error.toString());
+        }
+      });
+    } catch (_) {
+      if (kDebugMode) {
+        print("error on connecting to websocket.");
+      }
+    }
+  }
+
+  Future<void> sendCmd(String cmd) async {
+    if (connected == true) {
+      channel.sink.add(cmd);
+    } else {
+      channelConnect();
+      if (kDebugMode) {
+        print("Websocket is not connected.");
+      }
+    }
+  }
+
+  Future<void> toggleLED(bool value) async {
+    setState(() {
+      ledOn = value;
+    });
+    String cmd = ledOn ? "ledOn" : "ledOff";
+    sendCmd(cmd);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('NodeMCU Control'),
+        title: const Text("NodeMCU Controller"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'LED Status:',
-              style: TextStyle(fontSize: 24),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.topCenter,
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              child: connected
+                  ? const Text("WebSocket: CONNECTED")
+                  : const Text("DISCONNECTED"),
             ),
-            Switch(
-              value: isLEDTurnedOn,
-              onChanged: (value) {
-                setState(() {
-                  isLEDTurnedOn = value;
-                });
-                sendLEDCommand(value);
-              },
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding: const EdgeInsets.all(20),
+            child: Image.asset(
+              'assets/images/aiu_logo.png',
+              width: 200, // Adjust the width as per your requirements
+              height: 200, // Adjust the height as per your requirements
             ),
-            const SizedBox(height: 40),
-            const Text(
-              'Temperature:',
-              style: TextStyle(fontSize: 24),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.topCenter,
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'LED Status',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  CupertinoSwitch(
+                    value: ledOn,
+                    onChanged: toggleLED,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Temperature',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Text(
+                    '$temp °C',
+                    style: const TextStyle(fontSize: 32),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$currentTemperature °C',
-              style: const TextStyle(fontSize: 32),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: fetchTemperature,
-              child: const Text('Refresh'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
-  }
-
-Future<void> sendLEDCommand(bool isTurnedOn) async {
-    try {
-      String command = isTurnedOn ? 'ON' : 'OFF';
-      final url = Uri.parse('http://$nodeMCUIpAddress:$nodeMCUPort/$command');
-      await http.get(url);
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error sending LED command: $e');
-      }
-    }
-  }
-
-  Future<void> fetchTemperature() async {
-    try {
-      final url = Uri.parse('http://$nodeMCUIpAddress:$nodeMCUPort');
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        setState(() {
-          currentTemperature = response.body;
-        });
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error fetching temperature: $e');
-      }
-    }
   }
 }
